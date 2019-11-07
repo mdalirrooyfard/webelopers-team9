93aa52821d5a7a58aba0ec3f9f7df61c76d25558
fereshtah <fereshteh.forghani2012@gmail.com>
2019-11-07 15:18:05 +0330

    second step v1

--- first/urls.py
+++ first/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 from django.views.generic import TemplateView
 
-from first.views import homepage
+from first.views import homepage, signup
 
 urlpatterns = [
-    path('', homepage),
+    path('', homepage,name='home'),
+    path('signup/', signup, name='signup'),
 ]
--- first/views.py
+++ first/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+from first.forms import SignupForm
+
+
 def homepage(request):
     return render(request, 'Homepage.html')
+
+
+def signup(request):
+    # first_name = 'NaN'
+    # last_name = 'NaN'
+    # username = 'NaN'
+    # email = 'NaN'
+    # password1 = 'Nan'
+    # password2
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+            #return redirect('home')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
